9c63fe8633058b9e200d4a052d45f638fa8169a1
kurzzt <87642181+kurzzt@users.noreply.github.com>
2024-10-07 00:21:08 +0700

    cors config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { useContainer } from 'class-validator';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: 'hmtp2025.pemira.app',
+  });
   app.useGlobalPipes( new ValidationPipe({ transform: true }) );
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
